c95dfb70a5f631fda255db335172f945174149f8
ahmedomi <ahmedomarmiledi@gmail.com>
2022-05-23 09:51:57 +0100

    refactor:add fileds to models of employees

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from sqlalchemy import false
 
-# Create your models here.
+
+class Employee(models.Model):
+
+    TEAMS = (
+        ("DevOps_team", "DevOps_team"),
+        ("Dev_team", "Dev_team"),
+        ("Design_team", "Design_team")
+    )
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    birthday = models.DateField()
+    hired_on = models.DateTimeField()
+    position = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=30)
+    is_manager = models.BooleanField(default=False)
+    team = models.CharField(max_length=15, choices=TEAMS, null=True)
+
+    def __str__(self):
+        return self.name
