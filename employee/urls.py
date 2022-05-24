2d4e8ed3ceaef8c82f5907a5a62045738aff8dc4
ahmedomi <ahmedomarmiledi@gmail.com>
2022-05-24 20:13:31 +0100

    refactor:urls modified

--- employee/urls.py
+++ employee/urls.py
@@ -1,10 +1,14 @@
+from re import template
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
-  
+
+
 urlpatterns = [
+    path('', views.list, name='list_employees'),
+    path('add/', views.CreateEmployee, name='create_employee'),
     path('employees/', views.EmployeesList.as_view()),
     path('employees/<int:pk>/', views.EmployeeDetails.as_view()),
 ]
-  
+
 urlpatterns = format_suffix_patterns(urlpatterns)
